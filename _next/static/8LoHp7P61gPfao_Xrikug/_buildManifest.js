11ea2b2a72b4712a827652345f0ba68824347322
mulyant20 <96921234+mulyant20@users.noreply.github.com>
2022-12-06 15:17:06 +0000

    Deploying to gh-pages from @ mulyant20/mulyant20.github.io@bd0156187ba111edb6651f5116307e4b627f7d75 🚀

---
+++ _next/static/8LoHp7P61gPfao_Xrikug/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-5486f34730382d4c.js","static/chunks/pages/index-2622560f9d677115.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/vNsE9uzFhqqX3lQSNwi9h/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-5486f34730382d4c.js","static/chunks/pages/index-c92a32ec3121841d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
